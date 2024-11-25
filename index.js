280f2152afb3c872d2c0eba416a749e42bf30bfd
oper <seisenbaianuarr@gmail.com>
2024-11-25 10:55:48 +0500

    25.11.24

---
+++ index.js
@@ -0,0 +1,36 @@
+
+/*
+let a =+prompt("биринши сан енгизиниз")
+let b =+prompt("екинши сан енгизиниз")
+while (a , b) {
+    if(a + b) {
+    alert( a + b) 
+    }
+    if(a - b) {
+        alert( a - b) 
+        }
+        if(a * b) {
+            alert( a * b  ) 
+            }
+            if(a / b) {
+                alert( a /  b ) 
+                }
+}
+*/
+
+let pro =prompt("не алдыныз")
+let baga =+prompt("багасы")
+let a = 5000 
+let b = 10000
+let c = 20000
+while(pro, baga){
+    if( baga > 5000 || baga < 10000){
+        alert("сизге 10% женилдик" )
+    }
+    if( baga > 10000 || baga < 20000){
+        alert("сизге 20% женилдик" )
+    }
+    if( baga > 10000 || baga < 20000){
+        alert("сизге 20% женилдик" )
+    }
+}
